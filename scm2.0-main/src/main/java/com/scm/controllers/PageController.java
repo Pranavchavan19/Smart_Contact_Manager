87e9dd89351e8f9efa1d5ec40f960f01e8cb7b79
Pranavbc <pranav1952000@gmail.com>
2025-03-16 15:45:49 +0530

    update project

---
+++ scm2.0-main/src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,23 @@
+package com.scm.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+
+     System.out.println("home page h");
+
+     model.addAttribute("name", "pranav chavan");
+     model.addAttribute("chanel", "panu");
+     model.addAttribute("github", "https://github.com/Pranavchavan19");
+
+     return "home";
+    }
+   
+}
